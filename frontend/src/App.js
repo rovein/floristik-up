436f8a0118b3fed12a38da7a7ca6ea1a627d8e20
Roman Kuznetsov <roman.kuznetsov@nure.ua>
2021-05-02 00:16:25 +0300

    Removed unused classes amd imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,12 @@ import {Route, Switch, BrowserRouter, Redirect} from "react-router-dom";
 import SignIn from "./View/Components/Navigation/Login";
 import SignUp from "./View/Components/Navigation/SignUp";
 import Profile from "./View/Components/Navigation/Profile";
-import Search from "./View/Components/Navigation/Search";
 import {withTranslation} from "react-i18next";
 import Edit from "./View/Components/Navigation/Edit";
 
 import AddRoom from "./View/Components/Navigation/Room/AddRoom";
 import EditRoom from "./View/Components/Navigation/Room/EditRoom";
 
-import PickProcedure from "./View/Components/Navigation/PickProcedure";
-import CreateOrder from "./View/Components/Navigation/CreateOrder";
 import MoreInfo from "./View/Components/Navigation/MoreInfo";
 import AddFlower from "./View/Components/Navigation/Flower/AddFlower";
 import EditFlower from "./View/Components/Navigation/Flower/EditFlower";
@@ -37,9 +34,6 @@ class App extends React.Component {
 
             <Route path="/profile" component={Profile}/>
             <Route path="/edit" component={Edit}/>
-            <Route path="/search" component={Search}/>
-            <Route path="/view" component={PickProcedure}/>
-            <Route path="/pick" component={CreateOrder}/>
             <Route path="/more" component={MoreInfo}/>
 
             <Route path="/add_room" component={AddRoom}/>
